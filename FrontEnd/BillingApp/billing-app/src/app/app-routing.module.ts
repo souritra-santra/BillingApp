4814ec28e26d0e817fa11ba17f323fae63a9c75a
souritra-santra <souritrasantra@gmail.com>
2023-06-06 12:00:14 +0530

    message: routing added

--- FrontEnd/BillingApp/billing-app/src/app/app-routing.module.ts
+++ FrontEnd/BillingApp/billing-app/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewBillComponent } from './new-bill/new-bill.component';
+import { ReturnBillComponent } from './return-bill/return-bill.component';
+import { EditBillComponent } from './edit-bill/edit-bill.component';
+import { ViewAllBillsComponent } from './view-all-bills/view-all-bills.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'bill/new', component: NewBillComponent },
+  { path: 'bill/return', component: ReturnBillComponent },
+  { path: 'bill/edit', component: EditBillComponent },
+  { path: 'bill/view', component: ViewAllBillsComponent },
+  { path: 'inventory', component: InventoryComponent }
 
-    // { path: 'second-component', component: SecondComponent },
     // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
 
